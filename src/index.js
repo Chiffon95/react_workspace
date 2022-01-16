0d0e6f4952c2feec61fa06053344125962562030
Hyeon Hak Choi <apdlrm2006@nate.com>
2022-01-16 23:05:36 +0900

    JS in React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Fragment } from 'react';
 import ReactDOM from 'react-dom'; 
 
+const scissors = "가위"
+const rock = "바위"
+const paper = "보"
+
 ReactDOM.render(
   <>
   <Fragment>
@@ -9,8 +13,8 @@ ReactDOM.render(
   </Fragment>
   {/* 불필요한 div 태그를 줄일 수 있다. (<></> --- 축약형) */}
 
-  <h1 id="title">가위바위보</h1>
-    <button class="hand"> 가위</button>
-    <button class="hand"> 바위</button>
-    <button class="hand"> 보</button>
+  <h1 id="title">{scissors+rock+paper}</h1>
+    <button class="hand"> {scissors}</button>
+    <button class="hand"> {rock}</button>
+    <button class="hand"> {paper}</button>
  </>, document.getElementById('root'));
