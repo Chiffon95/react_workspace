0c38a78ef4a07519dc5f0ce76d3c4d3f548bc343
Hyeon Hak Choi <apdlrm2006@nate.com>
2022-01-16 22:45:59 +0900

    수정하기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom'; 
 
 ReactDOM.render(
-  <h1>안녕 리액트!</h1>,
-  document.getElementById('root')
-);
+  <>
+  <h1 id="title">가위바위보</h1>>
+  <button class="hand"> 가위</button>
+  <button class="hand"> 바위</button>
+  <button class="hand"> 보</button>
+ </>, document.getElementById('root'));
