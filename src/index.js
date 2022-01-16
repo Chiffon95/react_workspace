b38e605bde1ad47382a857b9488d2eb3cf097176
Hyeon Hak Choi <apdlrm2006@nate.com>
2022-01-16 22:57:16 +0900

    Fragment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom'; 
 
 ReactDOM.render(
   <>
-  <h1 id="title">가위바위보</h1>>
-  <button class="hand"> 가위</button>
-  <button class="hand"> 바위</button>
-  <button class="hand"> 보</button>
+  <Fragment>
+    <p>안녕</p>
+    <p>리액트</p>
+  </Fragment>
+  {/* 불필요한 div 태그를 줄일 수 있다. (<></> --- 축약형) */}
+
+  <h1 id="title">가위바위보</h1>
+    <button class="hand"> 가위</button>
+    <button class="hand"> 바위</button>
+    <button class="hand"> 보</button>
  </>, document.getElementById('root'));
